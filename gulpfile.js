8b28325d33699f9966d1efccf7970b1fe2ce3588
Taylor Gerring <taylor.gerring@gmail.com>
2016-06-10 21:54:00 -0400

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -17,53 +17,6 @@ gulp.task('clean', function() {
         .pipe(plugins.clean());
 });
 
-gulp.task('templates', ['scripts', 'styles'], function() {
-    return es.merge(
-
-        gulp.src(config.typeMap.html, { cwd: config.typePaths.templates.src }),
-
-        gulp.src(config.typeMap.jade, { cwd: config.typePaths.templates.src })
-        .pipe(plugins.jade({ pretty: (isProduction ? false : true) })))
-
-    .pipe(plugins.inject(
-            gulp.src(config.typePaths.styles.dest + config.GLOBSTAR, { read: false })
-            .pipe(plugins.order(config.styleOrder))
-            .pipe(plugins.using({ prefix: 'Injecting' })), {
-                addRootSlash: false,
-                ignorePath: config.basePaths.dest
-            }))
-        // .pipe(plugins.inject(
-        //   gulp.src(config.typePaths.scriptshead.dest + config.GLOBSTAR, {read: false}, {starttag: '<!-- inject:head:{{ext}} -->'})
-        //   .pipe(plugins.using({prefix: 'Injecting'})),
-        //     { addRootSlash: false, ignorePath: config.basePaths.dest })
-        // )
-        .pipe(plugins.inject(
-            gulp.src([config.typePaths.scripts.dest + config.GLOBSTAR], { read: false })
-            .pipe(plugins.order(config.scriptOrder))
-            .pipe(plugins.using({ prefix: 'Injecting' })), { addRootSlash: false, ignorePath: config.basePaths.dest }))
-
-    .pipe(plugins.size({ title: 'templates', showFiles: true, gzip: true }))
-        .pipe(isProduction ? gutil.noop() : plugins.connect.reload())
-        .pipe(gulp.dest(config.typePaths.templates.dest));
-});
-
-gulp.task('styles', function() {
-    return es.merge(
-
-        gulp.src(config.typeMap.less, { cwd: config.typePaths.styles.src })
-        .pipe(plugins.less()),
-
-        gulp.src(config.typeMap.css, { cwd: config.typePaths.styles.src }))
-
-    .pipe(plugins.autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
-        .pipe(isProduction ? plugins.csso() : gutil.noop())
-        .pipe(plugins.order(config.styleOrder))
-        .pipe(isProduction ? plugins.concat(config.appName + '.' + nicedate + '.css') : gutil.noop())
-        .pipe(plugins.size({ title: 'styles', showFiles: true, gzip: true }))
-        .pipe(isProduction ? gutil.noop() : plugins.connect.reload())
-        .pipe(gulp.dest(config.typePaths.styles.dest));
-});
-
 gulp.task('scripts', function() {
     return es.merge(
             es.merge(
@@ -87,13 +40,7 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest(config.typePaths.scripts.dest));
 });
 
-gulp.task('images', function() {
-    return gulp.src(config.appFiles.images, { cwd: config.typePaths.images.src })
-        .pipe(isProduction ? plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }) : gutil.noop())
-        .pipe(plugins.size({ title: 'imagemin', showFiles: false }))
-        .pipe(isProduction ? gutil.noop() : plugins.connect.reload())
-        .pipe(gulp.dest(config.typePaths.images.dest));
-});
+
 
 gulp.task('extras', function() {
     return gulp.src(config.appFiles.extras, { cwd: config.typePaths.extras.src })
@@ -111,45 +58,22 @@ gulp.task('bundle', function() {
         .pipe(gulp.dest('..'));
 });
 
-gulp.task('rename', ['templates'], function() {
+gulp.task('rename', function() {
     return gulp.src(config.typePaths.templates.dest + 'index.html')
         .pipe(isProduction ? plugins.rename('index.' + nicedate + '.html') : gutil.noop())
         .pipe(isProduction ? plugins.rename(config.appName + '.' + nicedate + '.html') : gutil.noop())
         .pipe(gulp.dest(config.typePaths.templates.dest));
 });
 
-gulp.task('watch', function() {
-    plugins.connect.server({
-        livereload: true,
-        port: config.SERVER_PORT,
-        root: config.basePaths.dest
-    });
 
-    gulp.watch(config.typePaths.styles.src + config.GLOBSTAR, ['styles']);
-    gulp.watch(config.typePaths.scripts.src + config.GLOBSTAR, ['scripts']);
-    gulp.watch(config.typePaths.templates.src + config.GLOBSTAR, ['templates']);
-    gulp.watch(config.typePaths.images.src + config.GLOBSTAR, ['images']);
-    gulp.watch(config.typePaths.extras.src + config.GLOBSTAR, ['extras']);
 
-});
-
-gulp.task('open', ['templates'], function() {
-    var uri = 'http://localhost:' + config.SERVER_PORT;
-    var sourceFile = config.basePaths.src + '../README.md';
-
-    gulp.src(sourceFile)
-        .pipe(isProduction ? gutil.noop() : plugins.open('', { url: uri }));
-});
 
 // Define the default task as a sequence of the above tasks
 // Additionally, enable production build on any task by adding "--prod"
 gulp.task('build', ['clean'], function() {
-    gulp.start('extras', 'scripts', 'styles', 'images', 'templates', 'rename');
+    gulp.start('extras', 'scripts', 'rename');
 });
 
-gulp.task('view', ['clean'], function() {
-    gulp.start('extras', 'scripts', 'styles', 'images', 'templates', 'watch', 'open');
-});
 
 gulp.task('default', function() {
     gulp.start('build');
